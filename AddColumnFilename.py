e6933b3572e75521e140be6041bd7eda689075e2
TruffleMedia <trufflemedia@gmail.com>
2018-01-04 14:55:38 -0500

    Create AddColumnFilename.py

---
+++ AddColumnFilename.py
@@ -0,0 +1,25 @@
+# 2017 09 06 John Blue Get Excel file name, insert column, 
+# add the Excel's filename into all the cells of that new column. 
+# This is used to help know where data originated.
+# see info at http://zetcode.com/articles/openpyxl/
+
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+import sys
+
+excelfilename=sys.argv[1]
+wb=load_workbook(excelfilename) #workbook from file
+ws=wb.worksheets[0] #first worksheet from workbook
+
+rmax = ws.max_row
+rmin = ws.min_row
+
+targetcolumn = ws.max_column+1
+
+# http://nullege.com/codes/search/openpyxl.worksheet.Worksheet.cell
+for arow in range(rmin, rmax+1):
+   ws.cell(row=arow, column=targetcolumn).value= excelfilename
+   
+newsavefile= "new_" + excelfilename
+wb.save(newsavefile)
