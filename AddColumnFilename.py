b574eec85b09c2754c4cb6d9773ca292771916f4
TruffleMedia <trufflemedia@gmail.com>
2018-01-04 15:01:52 -0500

    Update AddColumnFilename.py

--- AddColumnFilename.py
+++ AddColumnFilename.py
@@ -7,7 +7,7 @@
 # use
 # python AddColumnFilename.py excel-file.xlsx
 # 
-# This will create a file called new_ex/cel-file.xlsx in which the last
+# This will create a file called new_excel-file.xlsx in which the last
 # column of each row contains "excel-file.xlsx"
 
 from openpyxl import Workbook
